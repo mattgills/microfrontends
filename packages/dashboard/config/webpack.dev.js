3ac0ddf32dc8513e89825e12268dece516b69b73
Matthew Gillespie <matt.gillespie24@gmail.com>
2021-04-09 10:43:13 -0400

    Updates webpack configs

--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -6,27 +7,30 @@ const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8083/'
+    publicPath: 'http://localhost:8083/',
   },
   devServer: {
     port: 8083,
     historyApiFallback: {
-      index: '/index.html'
+      index: 'index.html',
     },
     headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
+      'Access-Control-Allow-Origin': '*',
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'dashboard',
       filename: 'remoteEntry.js',
       exposes: {
-        './DashboardApp': './src/bootstrap'
+        './DashboardApp': './src/bootstrap',
       },
-      shared: packageJson.dependencies
-    })
-  ]
-}
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
